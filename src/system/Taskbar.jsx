d8cd7ec6ecf1173bf6f07d98c8b63c47f1f0e2ea
sussybocca <babyyodacutefry@gmail.com>
2025-10-22 08:02:25 -0500

    Update Taskbar.jsx

--- src/system/Taskbar.jsx
+++ src/system/Taskbar.jsx
@@ -1,19 +1,34 @@
 // src/system/Taskbar.jsx
-import React from "react";
+import React, { useState } from "react";
 import { useSystemStore } from "../store/systemStore";
 import Windows from "./Windows";
 import Search from "./Search";
 
 export default function Taskbar() {
-  const { openApp } = useSystemStore();
-
-  // Base pinned apps
-  const pinnedApps = [
+  const { openApp, openApps, toggleMinimize, bringToFront } = useSystemStore();
+  const [pinnedApps, setPinnedApps] = useState([
     { name: "Explorer", icon: "/icons/file.svg" },
     { name: "Terminal", icon: "/icons/terminal.svg" },
     { name: "Web Bro OS Mini", icon: "/icons/mini-os.svg" },
-    { name: "WebBoe Browser", icon: "/icons/browser.svg" }
-  ];
+    { name: "WebBoe Browser", icon: "/icons/browser.svg" },
+  ]);
+
+  const togglePin = (appName, icon) => {
+    if (pinnedApps.find((a) => a.name === appName)) {
+      setPinnedApps(pinnedApps.filter((a) => a.name !== appName));
+    } else {
+      setPinnedApps([...pinnedApps, { name: appName, icon }]);
+    }
+  };
+
+  const handleAppClick = (appName) => {
+    const opened = openApps.find((a) => a.name === appName);
+    if (opened) {
+      toggleMinimize(opened.id);
+    } else {
+      openApp(appName);
+    }
+  };
 
   return (
     <div
@@ -30,22 +45,33 @@ export default function Taskbar() {
         zIndex: 9999,
       }}
     >
+      {/* Pinned apps */}
       <div style={{ display: "flex", gap: 6 }}>
-        {pinnedApps.map(app => (
+        {pinnedApps.map((app) => (
           <img
             key={app.name}
             src={app.icon}
             alt={app.name}
-            style={{ width: 36, height: 36, cursor: "pointer" }}
-            onClick={() => openApp(app.name)}
+            title={`Right-click to ${pinnedApps.find(a => a.name === app.name) ? "unpin" : "pin"}`}
+            style={{
+              width: 36,
+              height: 36,
+              cursor: "pointer",
+              opacity: openApps.find((a) => a.name === app.name)?.minimized ? 0.5 : 1,
+            }}
+            onClick={() => handleAppClick(app.name)}
+            onContextMenu={(e) => {
+              e.preventDefault();
+              togglePin(app.name, app.icon);
+            }}
           />
         ))}
       </div>
 
-      {/* Open windows */}
+      {/* Active Windows */}
       <Windows />
 
-      {/* Search bar */}
+      {/* Search */}
       <Search />
     </div>
   );
