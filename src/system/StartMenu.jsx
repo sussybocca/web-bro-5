4f5abfe06a2488029300fd94fc5738b2a4a05b45
sussybocca <babyyodacutefry@gmail.com>
2025-10-23 15:55:11 -0500

    Update StartMenu.jsx

--- src/system/StartMenu.jsx
+++ src/system/StartMenu.jsx
@@ -4,21 +4,28 @@ import { useSystemStore } from "../store/systemStore";
 
 export default function StartMenu() {
   const [open, setOpen] = useState(false);
-  // Access apps from the store
-  const apps = useSystemStore((s) => s.desktopApps || []);
+  const apps = useSystemStore((s) => s.apps);
+  const openApp = useSystemStore((s) => s.openApp);
 
   return (
-    <div className="start-menu-container">
-      <button className="start-button" onClick={() => setOpen(!open)}>
+    <div>
+      <button
+        className="start-button"
+        onClick={() => setOpen(!open)}
+      >
         🪟 Start
       </button>
 
       {open && (
-        <div className="start-menu">
+        <div className="start-menu bg-gray-900 text-white p-4 w-64 rounded shadow-lg">
           <AppSearch apps={apps} />
-          <div className="app-list">
+          <div className="app-list mt-2">
             {apps.map((app) => (
-              <div key={app.name} className="start-menu-item">
+              <div
+                key={app.id}
+                className="app-item hover:bg-gray-700 p-2 rounded cursor-pointer"
+                onClick={() => openApp(app.name)}
+              >
                 {app.name}
               </div>
             ))}
